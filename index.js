d15d0b5b8d43405e5bf9125094a1f7bfc9eb0690
Abogeerick <abogeerick@gmail.com>
2023-06-14 15:43:38 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+// Create a new header element
+const newHeader = document.createElement("h1");
+
+// Set the text content of the new header
+newHeader.textContent = "YOUR-NAME is the champion";
+
+// Set the ID attribute of the new header
+newHeader.id = "victory";
+
+// Replace the existing main element with the new header
+const mainElement = document.getElementById("main");
+mainElement.parentNode.replaceChild(newHeader, mainElement);
